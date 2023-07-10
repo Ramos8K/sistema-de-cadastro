cbeb3c7561f41826f881b8b356b21031d57b9345
unknown <gabriel.jr.2010@outlook.com>
2023-07-10 15:20:18 -0300

    Adicionando console log informando a conexao com o banco de dados

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log ("Rodando o sistema de gerenciamento de jogos")
 
 // mais um console log de teste:
 console.log("Enviando para o GitHub")
+console.log("A aplicação concectando ao banco de dados")
