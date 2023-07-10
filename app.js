0983af99be741a3331b796019c27989cac40bcdc
unknown <gabriel.jr.2010@outlook.com>
2023-07-10 15:37:40 -0300

    voltando atrás, quando foi criado o arquivo js com console

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
-
-// mais um console log de teste:
-console.log("Enviando para o GitHub")
-console.log("A aplicação concectando ao banco de dados")
